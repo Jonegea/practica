142b5d8200999f4a2a51f52d269d98a8db4e082d
jon_11_490 <jon_11_490rhotmail.com>
2022-11-11 20:41:27 -0300

    Recargando Hello

---
+++ HelloWordl.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+int n;
+
+	cout<<" Elige un numero par"<<endl;
+
+	cin>>n;
+
+	if ( n == 0){
+
+cout<<"es par";
+	}
+  
+    return 0; 
+}
